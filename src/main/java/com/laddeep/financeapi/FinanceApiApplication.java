720a15b54039909927143ba3fb7a30b7cf2a83a7
Nico Ramos <47761214+nicoramos28@users.noreply.github.com>
2021-01-30 12:23:15 -0300

    war 0.2

--- src/main/java/com/laddeep/financeapi/FinanceApiApplication.java
+++ src/main/java/com/laddeep/financeapi/FinanceApiApplication.java
@@ -10,13 +10,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @SpringBootApplication
 @Slf4j
-public class FinanceApiApplication {
+public class FinanceApiApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	StockService stockService;
@@ -46,5 +48,10 @@ public class FinanceApiApplication {
 	public void earningRunner(){
 		log.info("\n######################################## Start Finance-API Earning Runner ########################################");
 		earningService.getEarnings();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FinanceApiApplication.class);
 	}
 }
